87ffcb3a828e13ebd1b73ecd0b819feed46036b8
k03mad <k03.mad@gmail.com>
2021-12-07 01:27:40 +0300

    add mikrotik

--- app/index.js
+++ app/index.js
@@ -1,8 +1,17 @@
+import switch_ from './api/mikrotik/switch.js';
+import write from './api/mikrotik/write.js';
 import now from './utils/date/now.js';
 import ex from './utils/print/ex.js';
 import delay from './utils/promise/delay.js';
 
 const util = {
+    // api
+    mikrotik: {
+        switch: switch_,
+        write,
+    },
+
+    // utils
     date: {
         now,
     },
--- env.js
+++ env.js
@@ -1,1 +1,7 @@
-export default {};
+export default {
+    mikrotik: {
+        host: process.env.MIKROTIK_HOST,
+        user: process.env.MIKROTIK_USER,
+        password: process.env.MIKROTIK_PASSWORD,
+    },
+};
