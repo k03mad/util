b81b4dd1b855e4c6452d172546f231950e78ebd7
k03mad <k03.mad@gmail.com>
2021-12-08 18:13:08 +0300

    update post

--- app/api/mikrotik/post.js
+++ app/api/mikrotik/post.js
@@ -1,18 +1,20 @@
 import env from '../../../env.js';
 import got from '../../utils/request/got.js';
-import opts from './_opts.js';
+import defOpts from './_opts.js';
 
 const {mikrotik} = env;
 
 /**
  * @param {string} path
  * @param {object} json
+ * @param {object} opts
  * @returns {object}
  */
-export default async (path, json) => {
+export default async (path, json, opts) => {
     const {body} = await got(`https://${mikrotik.host}/rest/${path}`, {
-        ...opts,
+        ...defOpts,
         method: 'POST', json,
+        ...opts,
     });
 
     return body;
