68ff6a536d86aa78417add047636979314a3f2f3
k03mad <k03.mad@gmail.com>
2022-02-22 03:02:12 +0300

    update progress

--- app/utils/progress/start.js
+++ app/utils/progress/start.js
@@ -1,7 +1,25 @@
+import chalk from 'chalk';
+import cliProgress from 'cli-progress';
+
+const {blue, gray, green} = chalk;
+
 /**
- * @param {object} bar
+ * @param {string} name
  * @param {number} length
+ * @returns {object}
  */
-export default (bar, length) => {
+export default (name, length) => {
+    const options = {
+        format: `${blue(name)} ${green('[{bar}]')} {value}/{total} ${gray('{extra}')}`,
+        hideCursor: true,
+        stopOnComplete: true,
+        autopadding: true,
+        barCompleteChar: '#',
+        barIncompleteChar: '.',
+    };
+
+    const bar = new cliProgress.SingleBar(options);
     bar.start(length, 0, {extra: ''});
+
+    return bar;
 };
