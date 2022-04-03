78b62c2d513221ecd72024c5dc38a937eb1597ea
k03mad <k03.mad@gmail.com>
2022-04-03 23:20:04 +0300

    fix save

--- env.js
+++ env.js
@@ -9,7 +9,6 @@ export default {
     influx: {
         url: process.env.INFLUX_URL,
         db: process.env.INFLUX_DB,
-        request: process.env.INFLUX_STORE_REQUEST_STATS,
         ipPort: process.env.INFLUX_IP_PORT,
     },
     ipinfo: {
